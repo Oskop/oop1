f9a8ac5d73658b82aa6b8261cb9183c35e3e051c
Oskop <oskop17@gmail.com>
2019-03-07 15:26:25 +0700

    Tugas1 OOP1

---
+++ Pertemuan 1/tugas1.java
@@ -0,0 +1,7 @@
+public class tugas1 {
+  public static void main(String[] args) {
+    System.out.println("Mokhamad Wijaya");
+    System.out.println("17090092");
+    System.out.println("Kelas 4C");
+  }
+}
