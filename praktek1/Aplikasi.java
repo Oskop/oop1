e983a3043ada16f572335e763004cd8b2f6472ba
Oskop <oskop17@gmail.com>
2019-03-14 16:54:01 +0700

    Parameter Argumen (inputan Terminal)

--- praktek1/Aplikasi.java
+++ praktek1/Aplikasi.java
@@ -3,10 +3,12 @@ public class Aplikasi {
     Mahasiswa mhs1 = new Mahasiswa("17090092", "Oskop");
     Mahasiswa mhs2 = new Mahasiswa("17090093", "Kosfer");
     Mahasiswa mhs3 = new Mahasiswa("17090094", "Softa");
+    Mahasiswa mhs3 = new Mahasiswa(args[0], args[1]); // input saat di terminal
     // mhs1.nim = "19090092";
     // mhs1.nama = "Mokhamad";
-    System.out.println(mhs1.nim+" : "+mhs1.nama);
-    System.out.println(mhs2.nim+" : "+mhs2.nama);
-    System.out.println(mhs3.nim+" : "+mhs3.nama);
+    System.out.println(mhs1.getNim()+" : "+mhs1.getNama());
+    System.out.println(mhs2.getNim()+" : "+mhs2.getNama());
+    System.out.println(mhs3.getNim()+" : "+mhs3.getNama());
+    System.out.println(mhs4.getNim()+" : "+mhs4.getNama());
   }
 }
